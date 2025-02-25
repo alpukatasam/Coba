df1f02deef62cfb0fab11381abcf6700fe6c727d
alpukatasam <132274063+alpukatasam@users.noreply.github.com>
2025-02-25 12:48:05 +0700

    Update App.py

--- App.py
+++ App.py
@@ -1,23 +1,14 @@
 import requests
 import streamlit as st
 
+# API Key & CX
 API_KEY = "AIzaSyCAYdkTkdm-v2uNdUT-jEjqcsYKL7hjxek"
 CX = "61d16d0978b5a455e"
-QUERY = "spices"
 
-url = f"https://www.googleapis.com/customsearch/v1?q={QUERY}&cx={CX}&key={API_KEY}&searchType=image"
+# Judul Aplikasi
+st.title("Pencarian Gambar dengan Google Images")
 
-try:
-    response = requests.get(url)
-    data = response.json()
+# Input Kata Kunci dari User
+query = st.text_input("Masukkan kata kunci pencarian:", "")
 
-    if "items" in data:
-        image_url = data["items"][0]["link"]
-        st.image(image_url, caption="Hasil pertama dari Google Images")
-        st.write(f"[Lihat Gambar]({image_url})")
-    else:
-        st.warning("Tidak ditemukan hasil.")
-
-except Exception as e:
-    st.error(f"Terjadi kesalahan: {e}")
-    st.write("Respon API:", data)  # Menampilkan respon API
+if st.button("Cari Gambar") and query:
